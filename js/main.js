7b199d175aa8bd7eaae9bfec0945847e0d2ac1b0
Ing. Roberto J. Vargas <rvargas@rv-solutions.net>
2023-07-13 15:25:18 -0500

    Agregar Productos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,98 @@
-//Menu del Email version Desktop
+
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarrito = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container")
 
-menuEmail.addEventListener("click", toggleDesktopMenu)
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuIcon.addEventListener("click", toggleMobileMenu);
+menuCarrito.addEventListener("click", toggleCarritoAside)
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
 }
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+    mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
+
+}
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+function renderProducts(arr) {
+    for (const product of arr) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        const productImage = document.createElement("img");
+        productImage.setAttribute("src", product.image);
+    
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+    
+        const productinfoDiv = document.createElement("div");
+    
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+    
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+    
+        productinfoDiv.appendChild(productPrice);
+        productinfoDiv.appendChild(productName);
+    
+        const productinfofigure = document.createElement("figure");
+        const productImgCard = document.createElement("img");
+        productImgCard.setAttribute("src", "../icons/bt_add_to_cart.svg");
+    
+        productinfofigure.appendChild(productImgCard);
+    
+        productInfo.appendChild(productinfoDiv)
+        productInfo.appendChild(productinfofigure);
+    
+        productCard.appendChild(productImage);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    
+    }
+}
+
+renderProducts(productList);
+
+
+
+
+
